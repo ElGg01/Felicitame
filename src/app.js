e5193dc73127afd7af2675cdf36e99ebf4a18b03
ElGg01 <jcng.aa135@gmail.com>
2023-11-21 19:53:24 -0600

    Fix de CORS

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use(cors({
   // origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-  origin: "http://localhost:5173",
+  //origin: "http://localhost:5173",
+  origin: true,
   credentials: true
 }));
 
